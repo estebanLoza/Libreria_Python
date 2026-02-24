f4fdc34d489756f6a526dfec213d753d0b33e9de
estebanLoza <estebanm.hloza@gmail.com>
2026-02-24 16:02:42 -0600

    Empezando sobre el menu principal y a importar los modulos del archivo usurios a al archivo main para la navegación y también poniendo el menu principal que llevará el programa

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-#Gestión de biblioteca v1.1
+# Gestión de biblioteca v1.1
 
 from carpetas.datos import Libros
+from carpetas.usurios import Usuarios
+
+# Pruebas de imprimir el diccionario
+# for mostrar in Libros.items():
+#     print(f"{mostrar} \n")
 
 
-#Pruebas de imprimir el diccionario
-for mostrar in Libros.items():
-    print(mostrar)
+def main():
 
+    print("*" * 50)
+    print("*" + " BIBLIOTECA SAN FE".center(48) + "*")
+    print("*" + " " * 48 + "*")
+    print("*" * 50)
+    print("\n")
+
+    opcion = int(input(": "))
+
+    if opcion == 1:
+        Usuarios()
+
+
+if __name__ == "__main__":
+    main()
