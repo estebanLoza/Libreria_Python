0657a56e32103920ced5139670034e702b2a0fe1
estebanLoza <estebanm.hloza@gmail.com>
2026-03-07 11:12:11 -0600

    Refactor busquedaEscritores function for improved readability and user interaction

--- carpetas/escritores.py
+++ carpetas/escritores.py
@@ -5,46 +5,52 @@
 import json  # importación de los datos de autores
 import os
 import sys
+import random
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 
-
-
 ARCHIVO = os.path.join(os.path.dirname(__file__), "librosDatos.json")
 
-
-
+#Nos permite traer información de los libros disponibles
 def subirLibros():
     with open(ARCHIVO, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
-
-
 def busquedaEscritores():
-    
+
     libros = subirLibros()
+    
+    
     while True:
-        busqueda = input("🔍: ")
-        
-        for titulo, info in libros.items():
-            if busqueda == info["Autor"]:
-                print(f"{titulo}")
         
         print("\n")
-        back = int(input("(0 regresar, 1 buscar de nuevo): "))  
+        print(" "*50 + "*" * 50)
+        print(" "*50 + "*" + " Escribe el nombre del Escritor".center(48) + "*")
+        print(" "*50 + "*" + " " * 48 + "*")
+        print(" "*50 + "*" * 50)
+        print("\n")  
+            
+        busqueda = input("🔍: ")
+        contador = 1
+        for titulo, info  in libros.items():
+            if busqueda == info["Autor"]:
+                print("\n")
+                print(f"{contador}) {titulo}")
+                contador+=1
+        print("\n")
+        back = int(input("(0 regresar, 1 buscar de nuevo): "))
         if back == 0:
-            return #🟢 solo retorna, main.py retoma el control
+            return  # 🟢 solo retorna, main.py retoma el control
         elif back == 1:
             continue
-        
+
 
 # La regla es:
 
 # Si corres main.py → solo importa la función, no la ejecuta
 # Si corres escritores.py directamente → sí la ejecuta
 
-    
 if __name__ == "__main__":
     busquedaEscritores()
