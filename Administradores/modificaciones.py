6edf4373d9e3733508e81e3c4ad763f29a0aff34
estebanLoza <estebanm.hloza@gmail.com>
2026-03-12 21:08:13 -0600

    interfaz de usuario creada

--- Administradores/modificaciones.py
+++ Administradores/modificaciones.py
@@ -1,4 +1,4 @@
-# Esta parte es para la sección administrador
+# Administración de usuarios ,eleiminación , etc..
 
 import json  # importación de los datos creados en json
 import os  # Para poder acceder al sistema y así guardar los datos cada vez que inicie
@@ -15,3 +15,12 @@ def subirLibros():
 def guardarLibro(libros):
     with open(ARCHIVO, "w", encoding="utf-8") as f:
         json.dump(libros, f, ensure_ascii=False, indent=4)
+
+
+def admUsuarios():
+    print(" " * 50 + "*" * 50)
+    print(" " * 50 + "*" + "ADMISNISTRACIÓN DE USUASRIOS".center(48) + "*")
+    print(" " * 50 + "*" + " " * 48 + "*")
+    print(" " * 50 + "*" * 50)
+
+    usRoot = input("Usario: ")
