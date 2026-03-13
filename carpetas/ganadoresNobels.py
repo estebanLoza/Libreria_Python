16fda9213bebf512a4da6afc8c9bfddb3ed8ee72
estebanLoza <estebanm.hloza@gmail.com>
2026-03-12 21:11:27 -0600

    Eleminación de espacio muerto

--- carpetas/ganadoresNobels.py
+++ carpetas/ganadoresNobels.py
@@ -1,24 +1,18 @@
-#toma los datos del json de preimosNobel.json
-
+# toma los datos del json de preimosNobel.json
 
 
 import json
 import os
-import sys 
+import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 ARCHIVO = os.path.join(os.path.dirname(__file__), "premiosNobel.json")
 
+
 def subirLibros():
     with open(ARCHIVO, "r", encoding="utf-8") as f:
         return json.load(f)
-
-
-
-
-
-
 
 
 def ganadoresNobel():
@@ -26,7 +20,7 @@ def ganadoresNobel():
     print(" " * 50 + "*" + " PREMIOS NOBEL".center(48) + "*")
     print(" " * 50 + "*" + " " * 48 + "*")
     print(" " * 50 + "*" * 50)
-    
+
     arte = r"""
          .--------.
       /  .------,  \
@@ -44,11 +38,9 @@ def ganadoresNobel():
     for linea in arte.split("\n"):
         print(linea.center(150))
 
-
-
-    #mostar lista Ganadores Nobel
+    # mostar lista Ganadores Nobel
     libros = subirLibros()
-    
+
     contador = 1
 
     for autor, info in libros.items():
@@ -59,7 +51,7 @@ def ganadoresNobel():
         print(f"    * Motivo: {info["motivo"]}")
         contador += 1
     print("\n")
- 
+
     while True:
         try:
             back = int(input("0 regresar o 1 para buscar año de ganador: "))
@@ -68,8 +60,9 @@ def ganadoresNobel():
                 return
             elif back == 1:
                 try:
-                    year = int(input("Escribe el año especifico del ganador: "))
-                     
+                    year = int(
+                        input("Escribe el año especifico del ganador: "))
+
                     encontrado = False
 
                     for autor, info in libros.items():
@@ -84,13 +77,9 @@ def ganadoresNobel():
                 except ValueError:
                     print("Por favor escribe un año válido. \n")
 
-        
         except ValueError:
             print("Error: Por favor, ingrese el número 0 para regresar.\n")
 
 
-
-
-    
 if __name__ == "__main__":
     ganadoresNobel()
