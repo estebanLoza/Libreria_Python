2d62b4d5c27b0a49967182e424416e3e321dfecf
estebanLoza <estebanm.hloza@gmail.com>
2026-03-26 12:45:40 -0600

    Se crea clase libro

--- modelos/libro.py
+++ modelos/libro.py
@@ -1,14 +1,25 @@
 #Información necesaria que tendría los libros
 
 
-class Libro():
-    def __init__(self,titulo, autor, sinopsis, genero, isbn, anio, portada):
+
+
+class Libro:
+    def __init__(self,titulo, autor, sinopsis, genero, isbn, año, portada):
         self.titulo = titulo
         self.autor = autor
         self.sinopsis = sinopsis
         self.genero = genero
         self.isbn = isbn
-        self.anio = anio
+        self.año = año
         self.portada = portada
 
+    def __str__(self):
+        return f"{self.titulo} - {self.autor} ({self.anio})"
+    
+    def Origen_autor(self,autor):
+        return self.autor.lower() == autor.lower()
+    
+    def Origen_genero(self, genero):
+        return self.genero.lower() == genero.lower()
+    
 
