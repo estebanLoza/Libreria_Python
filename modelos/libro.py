984a423e811f2a568564e8bd25e9246e2096a175
estebanLoza <estebanm.hloza@gmail.com>
2026-03-25 22:36:28 -0600

    Se crea clase del archivo Libros y metodos

---
+++ modelos/libro.py
@@ -0,0 +1,14 @@
+#Información necesaria que tendría los libros
+
+
+class Libro():
+    def __init__(self,titulo, autor, sinopsis, genero, isbn, anio, portada):
+        self.titulo = titulo
+        self.autor = autor
+        self.sinopsis = sinopsis
+        self.genero = genero
+        self.isbn = isbn
+        self.anio = anio
+        self.portada = portada
+
+
