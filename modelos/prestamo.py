0db5028e6e19ec821df31b558fd80e68970e20d8
estebanLoza <estebanm.hloza@gmail.com>
2026-03-29 15:15:31 -0600

    Implementa la clase Prestamo con métodos para calcular multas y verificar estado de vencimiento

---
+++ modelos/prestamo.py
@@ -0,0 +1,38 @@
+#Indicación sobre los datos de prestamos
+
+
+
+from datetime import datetime
+
+"""
+    Esto por ahora es por si mi lógica esta mal
+    pero después agregare lo que necesite.
+    (como operaciones o lo que llegue a necesitar).
+
+"""
+
+class Prestamo:
+    def __init__(self, nombre, libro, fecha_vencimiento):
+        self.nombre = nombre
+        self.libro = libro
+        self.Fecha_vencimiento = fecha_vencimiento
+        self.activo = True
+    
+
+    def __str__(self):
+        return (
+            f"Usuario: {self.usuario.nombre}\n"
+            f"Libro: {self.libro}\n"
+            f"Vence: {self.Fecha_vencimiento}\n"
+            f"Activo: {self.activo}"
+        )
+    
+    
+    def estado_vencido(self):
+        return date.today() > self.Fecha_vencimiento
+
+    def calcular_multa(self,multa_por_dia = 10):
+        if self.estado_vencido():
+            dias = (date.today() - self.Fecha_vencimiento).days
+            return dias * multa_por_dia
+        return 0
