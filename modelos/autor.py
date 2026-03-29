c545093cff410a36c9ba06761b12148ce1153232
estebanLoza <estebanm.hloza@gmail.com>
2026-03-29 15:15:08 -0600

    Se agrega el atributo 'anio' a la clase Autor y se mejora el método __str__ para incluirlo

--- modelos/autor.py
+++ modelos/autor.py
@@ -2,10 +2,22 @@
 
 
 class Autor:
-    def __init__(self, nombre, nacionalidad, motivo):
+    def __init__(self, nombre, nacionalidad, anio, motivo):
         self.nombre = nombre
         self.nacionalidad = nacionalidad
         self.motivo = motivo
+        self.anio = anio 
+
 
     def __str__(self):
-        return f"Nombre:{self.nombre}\n Nacionalidad: {self.nacionalidad}\n Motivo:{self.motivo}"
+        return (
+            f"Nombre: {self.nombre}\n"
+            f"Nacionalidad: {self.nacionalidad}\n"
+            f"Año: {self.anio}\n"
+            f"Motivo: {self.motivo}"
+        )
+    
+    #Este metodo es para indicar el año especifico
+    def es_del_anio(self, anio):
+        return  self.anio == anio
+
