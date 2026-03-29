502fb1264f5ddd4bb5cf1bfce29f8c4cdff546f1
estebanLoza <estebanm.hloza@gmail.com>
2026-03-29 15:15:40 -0600

    Crea la clase Usuario con atributos y método __str__ para mostrar información del usuario

---
+++ modelos/usuario.py
@@ -0,0 +1,22 @@
+#Creamos los detalles que tienen los usuarios en la biblioteca
+#como en este caso sería los datros que tendrían para buscarlos
+
+
+
+class Usuario:
+    def __init__(self, nombre, id):
+        self.nombre = nombre
+        self.id = id
+        self.librosPretados = []
+
+
+    #Lo que mostrará de información al
+    # buscar al usuarios.
+    def __str__(self):
+        return (
+            f"Nombre: {self.nombre}\n"
+            f"ID: {self.id}\n"
+            f"Libros Prestados: {self.librosPrestados}\n"
+
+        )
+
