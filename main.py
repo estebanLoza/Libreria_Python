307ba2697ff96006e1121e441e86ef2295a1f893
estebanLoza <estebanm.hloza@gmail.com>
2026-04-04 18:36:43 -0600

    Remove unused banner display function and related commented code from main.py

--- main.py
+++ main.py
@@ -2,33 +2,7 @@
 from vistas.menu import Menu
 
 
-# Pruebas de imprimir el diccionario
-# for mostrar in Libros.items():
-#     print(f"{mostrar} \n")
-
-
-def mostrarBanner():
-    print(" " * 50 + "*" * 50)
-    print(" " * 50 + "*" + " BIBLIOTECA SAN FE".center(48) + "*")
-    print(" " * 50 + "*" + " " * 48 + "*")
-    print(" " * 50 + "*" * 50)
-
-    arte = r"""
-                                                 .--.                      .---.
-                                             .---|__|              .-.     |~~~|
-                                             .--|===|--|_          |_|     |~~~|--.
-                                             |  |===|  |'\     .---!~|  .--|   |--|
-                                             |%%|   |  |.'\    |===| |--|%%|   |  |
-                                             |%%|   |  |\.'\   |   | |__|  |   |  |
-                                             |  |   |  | \  \  |===| |==|  |   |  |
-                                             |  |   |__|  \.'\ |   |_|__|  |~~~|__|
-                                             |  |===|--|   \.'\|===|~|--|%%|~~~|--|
-                                             ^--^---'--^    `-'`---^-^--^--^---'--'
-    """
-    print(arte)
-
-
 if __name__ == "__main__":
-    mostrarBanner()
+
     menu = Menu()
     menu.ejecutar()
