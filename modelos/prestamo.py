db9767cc3de78584ce85e74d38a839326784920c
estebanLoza <estebanm.hloza@gmail.com>
2026-04-04 18:14:39 -0600

    Corrige errores en la clase Prestamo: ajusta el nombre de la variable 'fecha_vencimiento' y mejora el método __str__ para mostrar correctamente los datos del préstamo.

--- modelos/prestamo.py
+++ modelos/prestamo.py
@@ -3,7 +3,7 @@
 #Aquí hicimos una composición en vez de hacer una herencia.
 
 
-from datetime import datetime
+from datetime import date
 
 """
     Esto por ahora es por si mi lógica esta mal
@@ -16,24 +16,24 @@ class Prestamo:
     def __init__(self, nombre, libro, fecha_vencimiento):
         self.nombre = nombre
         self.libro = libro
-        self.Fecha_vencimiento = fecha_vencimiento
+        self.fecha_vencimiento = fecha_vencimiento
         self.activo = True
     
 
     def __str__(self):
         return (
-            f"Usuario: {self.usuario.nombre}\n"
+            f"Usuario: {self.nombre}\n"
             f"Libro: {self.libro}\n"
             f"Vence: {self.Fecha_vencimiento}\n"
             f"Activo: {self.activo}"
         )
     
     
-    def estado_vencido(self):
-        return date.today() > self.Fecha_vencimiento
+    def esta_vencido(self):
+        return date.today() > self.fecha_vencimiento
 
     def calcular_multa(self,multa_por_dia = 10):
-        if self.estado_vencido():
-            dias = (date.today() - self.Fecha_vencimiento).days
+        if self.esta_vencido():
+            dias = (date.today() - self.fecha_vencimiento).days
             return dias * multa_por_dia
         return 0
