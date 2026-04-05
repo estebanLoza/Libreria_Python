1c8d9375259d58fb10e3661c9b04feb3fc04ad4e
estebanLoza <estebanm.hloza@gmail.com>
2026-04-04 18:14:59 -0600

    Refactor main function: remove commented code and streamline execution flow to use Menu class for better structure.

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 # Gestión de biblioteca v1.1
+from vistas.menu import Menu
 
-from Administradores.modificaciones import main as adminMain
-from Administradores.usurios import Usuarios
-from carpetas.libros import LibrosCatalogo
-from carpetas.escritores import busquedaEscritores
-from carpetas.ganadoresNobels import ganadoresNobel
-from carpetas.librosGeneros import Generos
 
 # Pruebas de imprimir el diccionario
 # for mostrar in Libros.items():
@@ -33,51 +28,7 @@ def mostrarBanner():
     print(arte)
 
 
-def main():
-    #
-    # print("*" * 50)
-    # print("*" + " BIBLIOTECA SAN FE".center(48) + "*")
-    # print("*" + " " * 48 + "*")
-    # print("*" * 50)
-    # print("\n")
-    #
-    while True:
-
-        mostrarBanner()
-
-        print("1) Lista de libros disponibles")
-        print("2) Buscar por Escritores")
-        print("3) Ganadores de Premios Nobels")
-        print("4) Generos")
-        print("5) Administrador")
-        print("0) **Salir**")
-
-        try:
-
-            opcion = int(input(": "))
-
-            if opcion == 1:
-                LibrosCatalogo()
-                print("\n")
-
-            elif opcion == 2:
-                busquedaEscritores()
-                print("\n")
-
-            elif opcion == 3:
-                ganadoresNobel()
-            elif opcion == 4:
-                Generos()
-            elif opcion == 5:
-                adminMain()
-            elif opcion == 0:
-                break
-
-        except ValueError:
-            # Se ejecuta si la conversión a int() falla
-            print("Error: Por favor, ingrese un numero del 0 al 5. \n")
-            print("\n")
-
-
 if __name__ == "__main__":
-    main()
+    mostrarBanner()
+    menu = Menu()
+    menu.ejecutar()
