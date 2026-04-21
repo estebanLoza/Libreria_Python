7de6e8560f73059169d49dbdbcc79e071bcbef84
estebanLoza <estebanm.hloza@gmail.com>
2026-04-21 00:20:08 -0600

    Add Nobel Prize API endpoint to fetch and return authors' details in JSON format

---
+++ routes/nobel.py
@@ -0,0 +1,27 @@
+#Esto mostrará la sección de nobel (lista de json)
+
+
+
+from flask import Blueprint, jsonify
+from servicios.gestorNobel import GestorNobel
+
+
+
+nobel_bp = Blueprint("nobel", __name__)
+gestorNobel = GestorNobel()
+
+
+
+
+@nobel_bp.route("/api/nobel")
+def obtener_nobels():
+    nobels = [
+        {
+            "nombre": autor.nombre,
+            "nacionalidad": autor.nacionalidad,
+            "motivo": autor.motivo,
+            "anio": autor.anio
+        }
+        for autor in gestorNobel.autores
+    ]
+    return jsonify(nobels)
