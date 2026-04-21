4a36271bba8884f0c3029f6b41c121f7014d1fb3
estebanLoza <estebanm.hloza@gmail.com>
2026-04-21 00:19:53 -0600

    Uncomment nobel blueprint registration in app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from flask_cors import CORS
 
 
 from routes.libro import libro_bp
-# from routes.nobel import nobel_bp
+from routes.nobel import nobel_bp
 # from routes.usuarios import usuarios_bp
 # from routes.admin import admin_bp
-#
+
 
 app = Flask(__name__)
 
@@ -18,7 +18,7 @@ CORS(app)
 
 
 app.register_blueprint(libro_bp)
-# app.register_blueprint(nobel_bp)
+app.register_blueprint(nobel_bp)
 # app.register_blueprint(usuarios_bp)
 # app.register_blueprint(admin_bp)
 #
