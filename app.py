b580785f0d3747f9c8b1da3f4aa2eead62bdc689
estebanLoza <estebanm.hloza@gmail.com>
2026-04-23 10:43:05 -0600

    Refactorización en el orden del código

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 # Este archivo sirve para el llamado de flask con los datos que tengo para mostrarlos
 # la páqina web.
-
+#
 
 from flask import Flask
 from flask_cors import CORS
-
 
 from routes.libro import libro_bp
 from routes.nobel import nobel_bp
 # from routes.usuarios import usuarios_bp
 # from routes.admin import admin_bp
 
-
 app = Flask(__name__)
 
 CORS(app)
-
 
 app.register_blueprint(libro_bp)
 app.register_blueprint(nobel_bp)
